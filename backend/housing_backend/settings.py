e1e23bed03681b834cb5eb3e52893b24d95419d1
Tomáš Mikula <mail@tomasmikula.cz>
2022-11-09 10:41:08 +0100

    Clean code

--- backend/housing_backend/settings.py
+++ backend/housing_backend/settings.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_path: pathlib.Path = pathlib.Path("housing_backend") / "model.joblib"
+    model_path: pathlib.Path = pathlib.Path("model.joblib")
     database_url: str = "sqlite:///./sql_app.db"
 
 
